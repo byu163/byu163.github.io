49ccc2976d5ffda391f71c4ddc9502c462327557
byu163 <dolphin092011@gmail.com>
2021-04-12 13:14:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,12 @@
 <body>
     <div class="form">
         <ul class="tab-group">
-            
+            <li class="tab"><a href="#signup">Sign Up</a></li>
+            <li class="tab active"><a href="#login"></a></li>
         </ul>
+
+        <div class="tab-content">
+            
+        </div>
     </div>
 </body>
