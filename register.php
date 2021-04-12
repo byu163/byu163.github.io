05741e8bd6dea18ef964a53aa5b6635e992a813f
byu163 <dolphin092011@gmail.com>
2021-04-12 08:57:31 -0400

    Update register.php

---
+++ register.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>GR Live Register</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+</html>
